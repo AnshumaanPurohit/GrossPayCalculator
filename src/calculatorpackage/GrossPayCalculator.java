90511c9d826f9b5778dc95e1fc00cef275536566
AnshumaanPurohit <anshumaanpurohit555@gmail.com>
2024-07-25 13:50:31 +0530

    Get the number of hours worked

---
+++ src/calculatorpackage/GrossPayCalculator.java
@@ -0,0 +1,12 @@
+package calculatorpackage;
+
+import java.util.Scanner;
+
+public class GrossPayCalculator {
+    public static void main(String arg[]){
+        // 1. Get the number of hours works
+        System.out.println("Please Enter the number of  hours the employee worked");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextInt();
+    }
+}
